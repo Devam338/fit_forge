918d6c0c669c4fbed5293f379aa537fd9b995ba2
Devam <44005330+Devam338@users.noreply.github.com>
2026-06-18 10:36:22 -0400

    Update charts.tsx

--- fitforge/components/charts.tsx
+++ fitforge/components/charts.tsx
@@ -11,7 +11,7 @@ export function WeeklyCaloriesChart({ data }: { data: { day: string; consumed: n
           <YAxis />
           <Tooltip />
           <Legend />
-          <Bar dataKey="consumed" fill="#34c97b" radius={[4,4,0,0]} />
+          <Bar dataKey="consumed" fill="#34c97b" radius={[4,4,0,0]} /> 
           <Bar dataKey="burned" fill="#f97316" radius={[4,4,0,0]} />
         </BarChart>
       </ResponsiveContainer>
