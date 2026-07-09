46af46d4e9462b22a59355c557c733842e22e5f7
Devam <44005330+Devam338@users.noreply.github.com>
2026-07-09 11:28:05 -0400

    Increase LogOut icon size in sidebar

--- fitforge/components/sidebar.tsx
+++ fitforge/components/sidebar.tsx
@@ -43,7 +43,7 @@ export function Sidebar() {
       </div>
       <div className="sidebar-bottom">
         <div className="sidebar-row"><span>Theme</span><Moon size={18} /></div>
-        <div className="sidebar-row"><span className="row"><LogOut size={18} /> Sign out</span></div>
+        <div className="sidebar-row"><span className="row"><LogOut size={20} /> Sign out</span></div>
       </div>
     </aside>
   );
